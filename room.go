55110bb94efed51553ff9fb99a3f3e32b1727f00
Shandheap <shandheap@eventmobi.com>
2015-03-20 02:03:32 -0400

    Implement basic realtime chat

--- room.go
+++ room.go
@@ -1,8 +1,9 @@
 package main
 
-const (
-	socketBufferSize = 1024
-	messageBufferSize = 256
+import (
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
 )
 
 type room struct {
@@ -23,27 +24,34 @@ func (r *room) run() {
 		case client := <-r.join:
 			// joining
 			r.clients[client] = true
-		case client := r.leave:
+		case client := <-r.leave:
 			// leaving
 			delete(r.clients, client)
 			close(client.send)
-		}
-		case msg := r.forward:
+		case msg := <-r.forward:
 			// forward message to all clients
 			for client := range r.clients {
 				select {
 				case client.send <- msg:
 					// send the message
 				default:
+					// failed to send
 					delete(r.clients, client)
 					close(client.send)
 				}
 			}
+		}
 	}
 }
 
-var upgrader = &websocket.Upgrader(ReadBufferSize: socketBufferSize, WriteBufferSize: socketBufferSize)
-func (r *room) ServeHTTP(w *http.ResponseWriter, req *http.Request) {
+const (
+	socketBufferSize  = 1024
+	messageBufferSize = 256
+)
+
+var upgrader = &websocket.Upgrader{ReadBufferSize: socketBufferSize, WriteBufferSize: socketBufferSize}
+
+func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	socket, err := upgrader.Upgrade(w, req, nil)
 	if err != nil {
 		log.Fatal("ServeHTTP:", err)
@@ -51,8 +59,8 @@ func (r *room) ServeHTTP(w *http.ResponseWriter, req *http.Request) {
 	}
 	client := &client{
 		socket: socket,
-		send: make(chan []byte, messageBufferSize),
-		room: r,
+		send:   make(chan []byte, messageBufferSize),
+		room:   r,
 	}
 	r.join <- client
 	defer func() { r.leave <- client }()
@@ -63,8 +71,8 @@ func (r *room) ServeHTTP(w *http.ResponseWriter, req *http.Request) {
 func newRoom() *room {
 	return &room{
 		forward: make(chan []byte),
-		join: make(chan *client),
-		leave: make(chan *client),
-		clients: make(map[*client]bool)
+		join:    make(chan *client),
+		leave:   make(chan *client),
+		clients: make(map[*client]bool),
 	}
 }
