c66af335538e73bc0c695cd5cefb99800f8f9e07
Shandheap <shandheap@eventmobi.com>
2015-03-20 01:06:32 -0400

    Write class methods for room struct

--- room.go
+++ room.go
@@ -1,7 +1,70 @@
 package main
 
+const (
+	socketBufferSize = 1024
+	messageBufferSize = 256
+)
+
 type room struct {
-	// forward: A queue for incoming messages that
+	// forward: A channel for incoming messages that
 	// should be forwarded to other clients
 	forward chan []byte
+	// join: A channel for clients waiting to join this room
+	join chan *client
+	// leave: A channel for clients waiting to leave this room
+	leave chan *client
+	// clients: A map that contains all clients currently in this room
+	clients map[*client]bool
+}
+
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			// joining
+			r.clients[client] = true
+		case client := r.leave:
+			// leaving
+			delete(r.clients, client)
+			close(client.send)
+		}
+		case msg := r.forward:
+			// forward message to all clients
+			for client := range r.clients {
+				select {
+				case client.send <- msg:
+					// send the message
+				default:
+					delete(r.clients, client)
+					close(client.send)
+				}
+			}
+	}
+}
+
+var upgrader = &websocket.Upgrader(ReadBufferSize: socketBufferSize, WriteBufferSize: socketBufferSize)
+func (r *room) ServeHTTP(w *http.ResponseWriter, req *http.Request) {
+	socket, err := upgrader.Upgrade(w, req, nil)
+	if err != nil {
+		log.Fatal("ServeHTTP:", err)
+		return
+	}
+	client := &client{
+		socket: socket,
+		send: make(chan []byte, messageBufferSize),
+		room: r,
+	}
+	r.join <- client
+	defer func() { r.leave <- client }()
+	go client.write()
+	client.read()
+}
+
+func newRoom() *room {
+	return &room{
+		forward: make(chan []byte),
+		join: make(chan *client),
+		leave: make(chan *client),
+		clients: make(map[*client]bool)
+	}
 }
